8e523a7cc0aed2b9198aa16716f23514448ee742
Sahil Tiwari <162821589+tiwari02122003@users.noreply.github.com>
2024-12-06 00:55:52 +0530

    Empty repository

--- next_level/src/app/topics/BiharSsc.js
+++
@@ -1,6 +0,0 @@
-export default function  BIHARSSC ()  {
-    return <div>BIHAR SSC</div>;
-};
-  
-
-  
--- next_level/src/app/topics/Ntpc.js
+++
@@ -1,6 +0,0 @@
-export default function  NTPC ()  {
-    return <div>NTPC</div>;
-};
-  
-
-  
