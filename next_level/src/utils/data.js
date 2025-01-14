b4e9034454343ea89782236cc765762f3df19867
mr7895 <maneeshsinghrawat423@gmail.com>
2025-01-14 20:12:49 +0530

    Add new slab in ncert, fix YouTube section

--- next_level/src/utils/data.js
+++ next_level/src/utils/data.js
@@ -3,27 +3,27 @@ export const dataAccordingToBtnSelected = [
     RRB: [
       {
         title: "Video From Youtube RRB",
-        ytURl: "https://www.youtube.com/watch?v=D7dFe4vT2ig",
-      },
-      {
-        title: "Video From Youtube RRB",
-        ytURl: "https://www.youtube.com/watch?v=pDUPMrJ9hmc",
-      },
-      {
-        title: "Video From Youtube RRB",
         ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
       },
       {
         title: "Video From Youtube RRB",
-        ytURl: "https://www.youtube.com/watch?v=D7dFe4vT2ig",
+        ytURl: "https://www.youtube.com/watch?v=a-UPuJJHPbA",
       },
       {
         title: "Video From Youtube RRB",
-        ytURl: "https://www.youtube.com/watch?v=pDUPMrJ9hmc",
+        ytURl: "https://www.youtube.com/watch?v=G8OdUFfoSe0",
       },
       {
         title: "Video From Youtube RRB",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=KYW2t3yFf18",
+      },
+      {
+        title: "Video From Youtube RRB",
+        ytURl: "https://www.youtube.com/watch?v=17CJqk1Lw1I",
+      },
+      {
+        title: "Video From Youtube RRB",
+        ytURl: "https://www.youtube.com/watch?v=2oIrVjf0LbA",
       },
     ],
   },
@@ -31,33 +31,27 @@ export const dataAccordingToBtnSelected = [
     NTPC: [
       {
         title: "Video From Youtube NTPC",
-        ytURl:
-          "https://www.youtube.com/watch?v=BjProjejky0&list=PLQhp_jRk65mDlYJc24w3FanDrygDOhah2",
-        ytThumb: "https://img.youtube.com/vi/pDUPMrJ9hmc/0.jpg",
+        ytURl: "https://www.youtube.com/watch?v=qb6zkDSL6gU",
       },
       {
         title: "Video From Youtube NTPC",
-        ytURl: "https://www.youtube.com/watch?v=qb6zkDSL6gU&list",
+        ytURl: "https://www.youtube.com/watch?v=eNYi6jqG6e4",
       },
       {
         title: "Video From Youtube NTPC",
-        ytURl:
-          "https://www.youtube.com/watch?v=2oIrVjf0LbA&list=PLQhp_jRk65mDlYJc24w3FanDrygDOhah2&index=3",
+        ytURl: "https://www.youtube.com/watch?v=Q0X-wNMR0dQ",
       },
       {
         title: "Video From Youtube NTPC",
-        ytURl:
-          "https://www.youtube.com/watch?v=BjProjejky0&list=PLQhp_jRk65mDlYJc24w3FanDrygDOhah2",
-        ytThumb: "https://img.youtube.com/vi/pDUPMrJ9hmc/0.jpg",
+        ytURl: "https://www.youtube.com/watch?v=8PBXuF6p9uE",
       },
       {
         title: "Video From Youtube NTPC",
-        ytURl: "https://www.youtube.com/watch?v=qb6zkDSL6gU&list",
+        ytURl: "https://www.youtube.com/watch?v=a9vZqURXj_g",
       },
       {
         title: "Video From Youtube NTPC",
-        ytURl:
-          "https://www.youtube.com/watch?v=2oIrVjf0LbA&list=PLQhp_jRk65mDlYJc24w3FanDrygDOhah2&index=3",
+        ytURl: "https://www.youtube.com/watch?v=BjProjejky0",
       },
     ],
   },
@@ -65,27 +59,19 @@ export const dataAccordingToBtnSelected = [
     UP_PCS: [
       {
         title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=QQK_o08wcgo",
       },
       {
         title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=QH8JnS4TxBE",
       },
       {
         title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=BwdBK23fKp8",
       },
       {
         title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
-      },
-      {
-        title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
-      },
-      {
-        title: "Video From Youtube UP_PCS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=03l2fY-QQbk",
       },
     ],
   },
@@ -93,15 +79,15 @@ export const dataAccordingToBtnSelected = [
     BIHAR_SSC_GS: [
       {
         title: "Video From Youtube BIHAR_SSC_GS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=VhHRhKPTRwo",
       },
       {
         title: "Video From Youtube BIHAR_SSC_GS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=RTVhKb1DJgM",
       },
       {
         title: "Video From Youtube BIHAR_SSC_GS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=uLiFbG0Lr0Q",
       },
     ],
   },
@@ -109,15 +95,19 @@ export const dataAccordingToBtnSelected = [
     CURRENT_AFFAIRS: [
       {
         title: "Video From Youtube CURRENT_AFFAIRS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=50XZ3eAqQ2I",
       },
       {
         title: "Video From Youtube CURRENT_AFFAIRS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=9CHHuzwm5CY",
       },
       {
         title: "Video From Youtube CURRENT_AFFAIRS",
-        ytURl: "https://www.youtube.com/watch?v=GB2kRJlR9v4",
+        ytURl: "https://www.youtube.com/watch?v=L_ZvvcpH0b0",
+      },
+      {
+        title: "Video From Youtube CURRENT_AFFAIRS",
+        ytURl: "https://www.youtube.com/watch?v=K6l3yI8RG9E",
       },
     ],
   },
